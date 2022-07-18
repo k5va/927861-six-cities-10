0a1dab498441fba36730e045d31d80eadb6b9760
k5va <k5va@yandex.ru>
2022-07-18 18:51:42 +0300

    Added state to ReviewsForm

--- project/src/components/reviews-form/reviews-form.tsx
+++ project/src/components/reviews-form/reviews-form.tsx
@@ -1,9 +1,9 @@
-import {ChangeEvent, useState} from 'react';
+import React, {ChangeEvent, useState} from 'react';
 import {ReviewRate} from '../../const';
 
 function ReviewsForm() {
   const [text, setText] = useState('');
-  const [rate, setRate] = useState<ReviewRate>(ReviewRate.One);
+  const [currentRate, setRate] = useState(ReviewRate.get('terribly'));
 
   const onRateChange = (evt: ChangeEvent<HTMLInputElement>) => setRate(Number(evt.target.value));
   const onTextChange = (evt: ChangeEvent<HTMLTextAreaElement>) => setText(evt.target.value);
@@ -12,60 +12,26 @@ function ReviewsForm() {
     <form className="reviews__form form" action="#" method="post">
       <label className="reviews__label form__label" htmlFor="review">Your review</label>
       <div className="reviews__rating-form form__rating">
-        <input
-          onChange={onRateChange}
-          checked={rate === ReviewRate.Five}
-          className="form__rating-input visually-hidden" name="rating" value="5" id="5-stars" type="radio"
-        />
-        <label htmlFor="5-stars" className="reviews__rating-label form__rating-label" title="perfect">
-          <svg className="form__star-image" width="37" height="33">
-            <use xlinkHref="#icon-star"></use>
-          </svg>
-        </label>
-
-        <input
-          onChange={onRateChange}
-          checked={rate === ReviewRate.Four}
-          className="form__rating-input visually-hidden" name="rating" value="4" id="4-stars" type="radio"
-        />
-        <label htmlFor="4-stars" className="reviews__rating-label form__rating-label" title="good">
-          <svg className="form__star-image" width="37" height="33">
-            <use xlinkHref="#icon-star"></use>
-          </svg>
-        </label>
-
-        <input
-          onChange={onRateChange}
-          checked={rate === ReviewRate.Three}
-          className="form__rating-input visually-hidden" name="rating" value="3" id="3-stars" type="radio"
-        />
-        <label htmlFor="3-stars" className="reviews__rating-label form__rating-label" title="not bad">
-          <svg className="form__star-image" width="37" height="33">
-            <use xlinkHref="#icon-star"></use>
-          </svg>
-        </label>
-
-        <input
-          onChange={onRateChange}
-          checked={rate === ReviewRate.Two}
-          className="form__rating-input visually-hidden" name="rating" value="2" id="2-stars" type="radio"
-        />
-        <label htmlFor="2-stars" className="reviews__rating-label form__rating-label" title="badly">
-          <svg className="form__star-image" width="37" height="33">
-            <use xlinkHref="#icon-star"></use>
-          </svg>
-        </label>
-
-        <input
-          onChange={onRateChange}
-          checked={rate === ReviewRate.One}
-          className="form__rating-input visually-hidden" name="rating" value="1" id="1-star" type="radio"
-        />
-        <label htmlFor="1-star" className="reviews__rating-label form__rating-label" title="terribly">
-          <svg className="form__star-image" width="37" height="33">
-            <use xlinkHref="#icon-star"></use>
-          </svg>
-        </label>
+        {Array.from(ReviewRate.entries()).map(([title, rate]) => (
+          <React.Fragment key={rate}>
+            <input
+              onChange={onRateChange}
+              checked={currentRate === rate}
+              value={rate}
+              id={`${rate}-stars`}
+              className="form__rating-input visually-hidden" name="rating" type="radio"
+            />
+            <label
+              htmlFor={`${rate}-stars`}
+              title={title}
+              className="reviews__rating-label form__rating-label"
+            >
+              <svg className="form__star-image" width="37" height="33">
+                <use xlinkHref="#icon-star"></use>
+              </svg>
+            </label>
+          </React.Fragment>
+        ))}
       </div>
       <textarea
         value={text}
--- project/src/const/review-rate/review-rate.ts
+++ project/src/const/review-rate/review-rate.ts
@@ -1,9 +1,11 @@
-enum ReviewRate {
-  One = 1,
-  Two = 2,
-  Three = 3,
-  Four = 4,
-  Five = 5,
-}
+const ReviewRate: Map<string, number> = new Map(
+  [
+    ['perfect', 5],
+    ['good', 4],
+    ['not bad', 3],
+    ['badly', 2],
+    ['terribly', 1],
+  ]
+);
 
 export default ReviewRate;
