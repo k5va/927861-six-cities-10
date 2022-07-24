c78a4d0f6e00a6184acd30b39bb8a46915f8de55
k5va <k5va@yandex.ru>
2022-07-24 17:59:04 +0300

    Refactored actions

--- project/src/store/actions.ts
+++
@@ -1,8 +0,0 @@
-import {createAction} from '@reduxjs/toolkit';
-import {Cities} from '../const';
-import {Offer} from '../types';
-
-const setCity = createAction<{city: Cities}>('city/setCity');
-const setOffers = createAction<{offers: Offer[]}>('city/setOffers');
-
-export {setCity, setOffers};
