231fec4b091d54fe72b831ad2538dfaaf424d970
k5va <k5va@yandex.ru>
2022-08-10 15:28:31 +0300

    Added load current offer test

---
+++ project/src/store/actions/load-current-offer/load-current-offer.test.ts
@@ -0,0 +1,46 @@
+import {Action} from 'redux';
+import thunk, {ThunkDispatch} from 'redux-thunk';
+import MockAdapter from 'axios-mock-adapter';
+import {configureMockStore} from '@jedmao/redux-mock-store';
+import {api} from '../../../api';
+import {State} from '../../../types';
+import loadCurrentOffer from './load-current-offer';
+import {StatusCodes} from 'http-status-codes';
+import {ApiRoute} from '../../../const';
+import {mockId, mockOffer, mockOffers} from '../../../mocks';
+
+describe('Tests of loadCurrentOffer action', () => {
+  const mockAPI = new MockAdapter(api);
+  const middlewares = [thunk.withExtraArgument(api)];
+
+  const mockStore = configureMockStore<
+      State,
+      Action,
+      ThunkDispatch<State, typeof api, Action>
+    >(middlewares);
+
+  it('Should be loadCurrentOffer.fulfilled when server returns 200', async () => {
+    const store = mockStore();
+    const offerId = mockId();
+
+    mockAPI
+      .onGet(`${ApiRoute.Offers}/${offerId}`)
+      .reply(StatusCodes.OK, mockOffer())
+      .onGet(`${ApiRoute.Offers}/${offerId}/${ApiRoute.Near}`)
+      .reply(StatusCodes.OK, mockOffers())
+      .onGet(`${ApiRoute.Reviews}${offerId}`)
+      .reply(StatusCodes.OK, mockOffers());
+
+    Storage.prototype.getItem = jest.fn();
+
+    expect(store.getActions()).toEqual([]);
+
+    await store.dispatch(loadCurrentOffer({offerId}));
+    const actions = store.getActions().map(({type}) => type);
+    expect(actions).toEqual([
+      loadCurrentOffer.pending.type,
+      loadCurrentOffer.fulfilled.type
+    ]);
+    expect(Storage.prototype.getItem).toBeCalledTimes(3);
+  });
+});
