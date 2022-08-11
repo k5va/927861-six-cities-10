ab0967c5d43eb006bc660df13f413030fbe822c6
k5va <k5va@yandex.ru>
2022-08-12 01:01:45 +0300

    Added OffersList test

---
+++ project/src/components/offers-list/offers-list.test.tsx
@@ -0,0 +1,32 @@
+import {render, screen} from '@testing-library/react';
+import {createMemoryHistory} from 'history';
+import {HistoryRouter, OffersList} from '../../components';
+import {AuthStatus, NameSpace, OfferCardMode} from '../../const';
+import {mockOffers} from '../../mocks';
+import {Provider} from 'react-redux';
+import {configureMockStore} from '@jedmao/redux-mock-store';
+import {State} from '../../types';
+
+const mockStore = configureMockStore<State>();
+const store = mockStore({
+  [NameSpace.User]: {authStatus: AuthStatus.Auth},
+});
+
+describe('Component: OfferCard', () => {
+  it('should render correctly', () => {
+    const history = createMemoryHistory();
+    const offers = mockOffers();
+
+    render(
+      <Provider store={store}>
+        <HistoryRouter history={history}>
+          <OffersList offers={offers} mode={OfferCardMode.Cities} />
+        </HistoryRouter>
+      </Provider>
+    );
+
+    for (const offer of offers) {
+      expect(screen.getByText(new RegExp(offer.title, 'i'))).toBeInTheDocument();
+    }
+  });
+});
