c797852ffca82118937aa7996ac7156fbc6edd1a
k5va <k5va@yandex.ru>
2022-08-16 18:06:11 +0300

    Added header to NotFound page

--- project/src/pages/not-found/not-found.tsx
+++ project/src/pages/not-found/not-found.tsx
@@ -1,27 +1,16 @@
-import {Link} from 'react-router-dom';
-import {AppRoute} from '../../const';
+import {Header, SVGSymbols} from '../../components';
 
 function NotFound(): JSX.Element {
   return (
-    <div className="page page--gray page--main">
-      <header className="header">
-        <div className="container">
-          <div className="header__wrapper">
-            <div className="header__left">
-              <Link className="header__logo-link header__logo-link--active" to={AppRoute.Root}>
-                <img className="header__logo" src="img/logo.svg" alt="6 cities logo"
-                  width="81" height="41"
-                />
-              </Link>
-            </div>
-          </div>
-        </div>
-      </header>
-
-      <main className="page__main page__main--index">
-        <h1 className='login__title'>404 Not Found</h1>
-      </main>
-    </div>
+    <>
+      <SVGSymbols />
+      <div className="page page--gray page--main">
+        <Header />
+        <main className="page__main page__main--index">
+          <h1 className='login__title'>404 Not Found</h1>
+        </main>
+      </div>
+    </>
   );
 }
 
