2b133ab8f419c2697ede32e26b45c774c866aee9
k5va <k5va@yandex.ru>
2022-08-17 00:47:25 +0300

    Added MIN_REVIEWS_NUM to mockReviews

--- project/src/mocks/mock-reviews/mock-reviews.ts
+++ project/src/mocks/mock-reviews/mock-reviews.ts
@@ -2,7 +2,11 @@ import {datatype} from 'faker';
 import {mockReview} from '..';
 
 const MAX_REVIEWS_NUM = 10;
+const MIN_REVIEWS_NUM = 2;
 
-const mockReviews = () => Array.from(new Array(datatype.number(MAX_REVIEWS_NUM)), mockReview);
+const mockReviews = () => Array.from(
+  new Array(datatype.number({min: MIN_REVIEWS_NUM, max: MAX_REVIEWS_NUM})),
+  mockReview
+);
 
 export default mockReviews;
